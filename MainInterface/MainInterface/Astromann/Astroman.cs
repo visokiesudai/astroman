36e0691f58a9481f5436225c3a5896e2cd6c6403
visokiesudai <36403631+visokiesudai@users.noreply.github.com>
2018-02-12 12:24:47 +0200

    Alpha 0.0.1

---
+++ MainInterface/MainInterface/Astromann/Astroman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainInterface.Astromann
+{
+    public partial class Astroman : Form
+    {
+        public Astroman()
+        {
+            InitializeComponent();
+        }
+
+        private void Astroman_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
