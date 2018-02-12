bfadb5b7c13276f194638023ce86e1955f82034c
visokiesudai <36403631+visokiesudai@users.noreply.github.com>
2018-02-12 12:28:43 +0200

    Add files via upload

---
+++ MainInterface/MainInterface/Minesweeper/MinesweeperForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainInterface.Minesweeper
+{
+    public partial class MinesweeperForm : Form
+    {
+        private int width = 20;
+        private int height = 20;
+        private int mines = 50;
+        
+
+        public MinesweeperForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MinesweeperForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
